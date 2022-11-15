74f0dbbac354d9bc5d703aef6e5e78eb898149db
joonhokil <kiljh@me.com>
2022-11-15 11:08:05 +0900

    STEP1

--- NumberBaseball/main.swift
+++ NumberBaseball/main.swift
@@ -7,5 +7,40 @@
 
 import Foundation
 
-print("Hello, World!")
 
+var count = 9
+let answers = get3Numbers()
+for tryCount in 1...count {
+    let remainCount = count - tryCount
+    let challenge = get3Numbers()
+    var strikeCount = 0
+    var ballCount = 0
+    for i in 0...2 {
+        if answers[i] == challenge[i] {
+            strikeCount += 1
+        }
+        if answers.contains(challenge[i]) {
+            ballCount += 1
+        }
+    }
+    ballCount -= strikeCount
+    print("임의의 수 : \(challenge[0]) \(challenge[1]) \(challenge[2])")
+    print("\(strikeCount) 스트라이크, \(ballCount) 볼")
+    if remainCount == 0 && strikeCount != 3 {
+        print("컴퓨터 승리...!")
+    }
+    print("남은 기회 : \(count - tryCount)")
+}
+
+func get3Numbers() -> [Int] {
+    var numbers = [Int]()
+    
+    while numbers.count < 3 {
+        let randomNumber = Int.random(in: 1...9)
+        if !numbers.contains(randomNumber) {
+            numbers.append(randomNumber)
+        }
+    }
+    
+    return numbers
+}
